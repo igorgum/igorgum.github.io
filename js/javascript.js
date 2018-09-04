b50618deb97063e02803001b8e2339b2beffae53
Igor Duarte <igorfun@hotmail.com>
2018-09-04 00:24:55 -0300

    adicionado carrossel

--- js/javascript.js
+++ js/javascript.js
@@ -2,4 +2,27 @@ $(document).ready(function() {
   $('.parallax').parallax();
   $('.scrollspy').scrollSpy();
   $(".button-collapse").sideNav({closeOnClick: true});
+  $('.carousel').carousel();
+  $('.materialboxed').materialbox();
 });
+
+
+//drag cancel e redirect
+var dragging = false;
+$(".carousel").on("touchmove", function(){
+      dragging = true;
+});
+$(".carousel").on("touchend", function(){
+      if (dragging)
+		  dragging = false;
+          return;
+	$(".carousel").on("touchstart", function(){
+		dragging = false;
+	});
+});
+
+function redirecionar(link){
+	if(dragging===false){
+		window.location = link;
+	}
+}
